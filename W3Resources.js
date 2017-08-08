10ce53c4e5b04c08b50086eece71ce168c980a9a
ramyatata <Ramyatata007@gmail.com>
2017-08-08 16:13:36 -0500

    Update W3Resources.js

--- W3Resources.js
+++ W3Resources.js
@@ -83,6 +83,13 @@ function range_Iterative(n1, n2){
   return results;
 }
 
+/*5. Write a JavaScript program to compute the exponent of a number. Go to the editor
+Note : The exponent of a number says how many times the base number is used as a factor.
+82 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent */
+//Recursive
+
+//Iterative
+
 
 
 
